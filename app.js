578ad4f10d9893116468fecff423adfb87536f4b
Rrezon <112092689+Rrez44@users.noreply.github.com>
2024-01-06 12:57:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
 const app = express();
 const server = http.createServer(app);
+const session = require('express-session');
 const io = socketIo(server);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -13,12 +14,36 @@ app.use(cors());
 const port = process.env.PORT || 4000;
 let users = {};
 let userCount = 0; 
-////////////////////////////////// sign up 
+
+
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/////////////////////////////// this is the session code, it will create a session for logged users
+
+app.use(session({
+    secret: '2514677942', 
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false, 
+        maxAge: 1000 * 60 * 60 * 24 
+    }
+}));
+
+
+function isAuthenticated(req, res, next) {
+    if (req.session.username) {
+        return next();
+    }
+    // if the user is not logged in, redirect them to the login page
+    return res.redirect('/login');
+}
+
+////////////////////////////////////// signup
 
 app.post('/signup', (req, res) => {
     const { username, password, email } = req.body;
@@ -46,13 +71,43 @@ app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
     if (users[username] && users[username].password === password) {
-        res.status(200).json({ success: true, message: 'Login successful' });
+
+        req.session.username = username;
+
+        req.session.save(err => {
+            if (err) {
+                console.error('Session save error:', err);
+                res.status(500).json({ success: false, message: 'Internal server error' });
+
+            } else {
+                res.status(200).json({ success: true, message: 'Login successful' });
+            }
+        });
     } else {
-        res.status(401).json({ success: false, message: 'Unauthorized' });
+        res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
 });
 
-//////////////////////////
+////////////////////////////////// log out when i make it
+
+app.post('/logout', (req, res) => {
+    // destroy the current  session
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                // error case
+                console.error('Error destroying session:', err);
+                res.status(500).send('Error occurred during logout');
+            } else {
+                res.status(200).send('Logged out successfully');
+            }
+        });
+    } else {
+        res.status(200).send('No session to destroy');
+    }
+});
+
+//////////////////////////   SITE SERVING HAPPENS HERE  ///////////////////////////
 
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'signup.html'));
@@ -62,15 +117,32 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 // this serves the chat page
-app.get('/chat', (req, res) => {
+app.get('/chat', isAuthenticated,(req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'chat.html'));
 });
 
+app.get('/home', isAuthenticated,(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'homepage.html'));
+});
+/////
+app.get('/getsession', (req, res) => {
+    if (req.session.username) {
+        res.json({ isLoggedIn: true, username: req.session.username });
+    } else {
+        res.json({ isLoggedIn: false });
+    }
+});
 io.on('connection', (socket) => {
-    userCount++; 
+    userCount++;
     io.emit('user count', userCount); 
 
     console.log('A user connected');
+
+
+    socket.on('send image', (data) => {
+        const imageData = data.imageData;
+        io.emit('received image', { imageData: imageData });
+    });
 
     socket.on('chat message', (msg) => {
         console.log('Message:', JSON.stringify(msg));
